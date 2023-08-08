01e8fad674b042c1e655f04d7a91910e0a78b1d2
msnkr <mikyle.designs@gmail.com>
2023-08-08 12:58:55 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const burgerBtn = document.querySelector(".burger-btn");
 const navList = document.querySelector(".navbar-list-items");
+const customerGradient = document.querySelectorAll(".customer-gradient");
 
 burgerBtn.addEventListener("click", () => {
   navList.classList.toggle("hidden");
